f923fc8ce69e900a41fba9d20e93b20e4112096c
RodSan <rodrigosantos@gmail.com>
2026-05-10 14:15:24 -0300

    add Blackbox nav link with config.js for external URLs

---
+++ config.js
@@ -0,0 +1,5 @@
+// External site URLs. Consumed by [data-link] elements via script.js.
+const SITE = {
+    blackbox: "https://0xrodsan.github.io/blackbox/",
+    blackboxPt: "https://0xrodsan.github.io/blackbox/pt/"
+};
